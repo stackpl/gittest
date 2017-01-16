24dc2dc4aef6a485f21b3b3eebdbdb68011adfa2
Dariusz Chilimoniuk <darek@wave.pl>
2017-01-17 00:15:00 +0100

    Some improvments

--- cogito.sh
+++ cogito.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-watch -d -t -n1 -c '
-	STATUS=$(unbuffer git status -s -u) ;
+watch -t -n1 -c '
+	STATUS=$(git status -s -u) ;
 	if [ $(echo $STATUS | wc -w) -gt 0 ] ;
 	then
-		unbuffer git status -s -u;
+		unbuffer git status -s -u --column;
 		#unbuffer echo $STATUS;
-		unbuffer echo -e ---------------------------------;
+		echo "________ ADD: git add <..>,  UDATE: git add -u,  RESET: git reset <..> ________" ;
+		echo "\n";
 	fi
-	unbuffer git branch --all -vv;
-	unbuffer echo -e \\n;
-	unbuffer git log --graph --oneline --decorate=short -n 40 | 
-	sed -e "s/\x31\x68\x1B\x3D\x0D//;s/\x31\x6C\x1B\x3E//" |
+	git branch --all -vv --abbrev=5 --color;
+	echo "\n";
+	git log --graph --oneline --decorate=short -n 25 --abbrev=5 --color | 
+	#sed -e "s/\x31\x68\x1B\x3D\x0D//;s/\x31\x6C\x1B\x3E//" |
 	cat ;
 	'
 
