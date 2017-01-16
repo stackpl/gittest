237bc44c979da56cdc32853cbe1c4d1b42ba352d
Dariusz Chilimoniuk <darek@wave.pl>
2017-01-17 00:45:11 +0100

    Some another improvments

--- cogito.sh
+++ cogito.sh
@@ -9,7 +9,8 @@ watch -t -n1 -c '
 		echo "________ ADD: git add <..>,  UDATE: git add -u,  RESET: git reset <..> ________" ;
 		echo "\n";
 	fi
-	git branch --all -vv --abbrev=5 --color;
+	#git branch --all -vv --abbrev=5 --color;
+	git branch --all --color;
 	echo "\n";
 	git log --graph --oneline --decorate=short -n 25 --abbrev=5 --color | 
 	#sed -e "s/\x31\x68\x1B\x3D\x0D//;s/\x31\x6C\x1B\x3E//" |
