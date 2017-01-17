03ff962d001dbab961eeaf87b57d3f746428af42
Dariusz Chilimoniuk <darek@wave.pl>
2017-01-17 22:44:25 +0100

    branch name when STAGED/UNSTAGED area appear

--- cogito.sh
+++ cogito.sh
@@ -4,15 +4,17 @@ watch -t -n1 -c '
 	STATUS=$(git status -s -uno);
 	if [ $(echo $STATUS | wc -w) -gt 0 ] ;
 	then
-		echo "  .----- STAGED  (ready to \"git commit\")";
-		echo " / .--- UNSTAGED";
+		git branch --points-at HEAD --color;
+		echo "";
+		echo ".----------- STAGED  (ready to commit)";
+		echo "|  .------- UNSTAGED";
 		echo "| /";
-		echo "||";
 		unbuffer git status -s -uno;
+		echo "";
 	else
-		echo ""
 	        git branch --color;
 	fi
+	echo "";
 	git log --all --graph --oneline --decorate -n 50 --abbrev=5 --color;
 	'
 
