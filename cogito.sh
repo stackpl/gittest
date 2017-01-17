f901880e0f1944aca7901fe2d3917644840c19aa
Dariusz Chilimoniuk <darek@wave.pl>
2017-01-17 01:12:01 +0100

    working tree improvements

--- cogito.sh
+++ cogito.sh
@@ -4,13 +4,16 @@ watch -t -n1 -c '
 	STATUS=$(git status -s -u) ;
 	if [ $(echo $STATUS | wc -w) -gt 0 ] ;
 	then
+		echo "___________________ WORKING TREE CHANGES SINCE INDEX __________________________" ; 
 		unbuffer git status -s -u -b --column;
 		#unbuffer echo $STATUS;
-		echo "________ ADD: git add <..>,  UDATE: git add -u,  RESET: git reset <..> ________" ;
-		echo "\n";
+		echo "_______________________________________________________________________________" ;
+	else
+	        #git branch --all -vv --abbrev=5 --color;
+	        git branch --all --color;
 	fi
 	#git branch --all -vv --abbrev=5 --color;
-	git branch --all --color;
+	#git branch --all --color;
 	echo "\n";
 	git log --graph --oneline --decorate=short -n 25 --abbrev=5 --color | 
 	#sed -e "s/\x31\x68\x1B\x3D\x0D//;s/\x31\x6C\x1B\x3E//" |
