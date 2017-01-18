80288a2dd299e04d42abe4a8cd11d6dc17404267
Dariusz Chilimoniuk <darek@wave.pl>
2017-01-18 21:45:08 +0100

    Second (non watching) script

---
+++ cogito2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+STATUS=$(unbuffer git status -s -uno);
+if [ $(echo $STATUS | wc -w) -gt 0 ] ; 
+then
+	git branch --points-at HEAD --color;
+	echo "";
+	echo ".----------- STAGED  (ready to commit)";
+	echo "|  .------- UNSTAGED";
+	echo "| /";
+	echo "$STATUS";
+	echo "";
+else
+        git branch --color;
+fi
+echo "";
+git log --all --graph --oneline --decorate -n 50 --abbrev=5 --color | cat;
