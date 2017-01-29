1cbf0d6f85e5020c86dce9d38d78ee7cb6e7d061
Dariusz Chilimoniuk <darek@wave.pl>
2017-01-29 02:28:49 +0100

    functionality divided to functions

--- cogito2.sh
+++ cogito2.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-STATUS=$(unbuffer git status -s -uno);
-if [ $(echo $STATUS | wc -w) -gt 0 ] ; 
+gits() {
+	git status -s -uall --ignored
+}
+
+git_status_tracked() {
+	if [ `echo "$1" | grep ^[^?!][^?!] | wc -l` -gt 0 ]
+	then
+		echo "       Changes in tracked files:";
+        	echo "+---------+---------+------------------";
+        	echo "|  Staged | Unstaged|  File name";
+        	echo "+---------+---------+------------------";
+        	echo "$1" \
+		| grep ^[^?!][^?!] \
+		| sed -r 's/(.)(.)\s*(.+)/|    \1    |    \2    | \3/' ;
+		echo "";
+	fi
+}
+
+git_status_untracked() {
+        if [ `echo "$1" | grep ^[?][?] | wc -l` -gt 0 ]
+	then
+		echo "+---------------------"
+        	echo "|  Untracked";
+        	echo "+---------------------";
+        	echo "$1" | \
+        	grep ^[?][?] | \
+        	sed -r 's/[?][?]\s*(.+)/| \1/' ;
+		echo "";
+	fi
+}
+
+STATUS=$(gits)
+if [ $(echo $STATUS | wc -w) -gt 0 ]  
 then
-	git branch --points-at HEAD --color;
+	git branch --points-at HEAD --color | tr -d '\n'; echo "  (current branch)";
 	echo "";
-	echo ".----------- STAGED  (ready to commit)";
-	echo "|  .------- UNSTAGED";
-	echo "| /";
-	echo "$STATUS";
-	echo "";
+	git_status_tracked "$STATUS";
+	git_status_untracked "$STATUS";
 else
         git branch --color;
 fi
